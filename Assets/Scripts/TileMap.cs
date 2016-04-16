84a69cf7f4624c34ab52ad9f597381b797da7de3
Rob Hailman <robhailman@gmail.com>
2016-04-16 18:46:35 -0400

    Basic tile map

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,35 @@
+﻿public enum TileContents
+{
+    EMPTY_TILE,
+    WALL
+}
+
+public class Tile
+{
+    public TileContents Contents;
+    public bool Visible;
+    public TileContents KnownContents;
+
+    public Tile(TileContents contents)
+    {
+        Contents = contents;
+        KnownContents = TileContents.EMPTY_TILE;
+    }
+    public void SetVisible()
+    {
+        KnownContents = Contents;
+        Visible = true;
+    }
+
+    public void SetInvisible()
+    {
+        Visible = false;
+    }
+}
+
+public class TileMap
+{
+
+    public Tile[,] TileArray;
+
+}
