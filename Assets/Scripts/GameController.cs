4764d235f2597d66822fa397dfa059447e75aef0
Rob Hailman <robhailman@gmail.com>
2016-04-17 16:03:07 -0400

    ... the rest of the last commit
    It's not much game controlling without committing the game controller.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -26,6 +26,14 @@ public enum ToolState
     ACTUATOR,
     PUSHER
 }
+
+public enum GameState
+{
+    MOVEMENT,
+    SELECTION,
+    WAIT,
+    NOTIFICATION
+}
 #endregion
 
 public class GameController : MonoBehaviour
@@ -47,10 +55,13 @@ public class GameController : MonoBehaviour
 
     public Color ActiveColour;
     public Color InactiveColour;
+    public Color ErrorColour;
 
     public Text[] SensorText;
     public Text[] ChassisText;
     public Text[] ToolText;
+    public Text PointerText;
+    public Text CountText;
 
     public GameObject PrefabTileSprite;
 
@@ -66,7 +77,7 @@ public class GameController : MonoBehaviour
     #endregion
 
     #region Private Attributes
-    private int PlayerTicks;
+    private int playerTicks;
 
     private SpriteDefinitions spriteDefinitions;
     private Sprite[] headSpriteArray;
@@ -75,9 +86,15 @@ public class GameController : MonoBehaviour
     private SensorState sensorState;
     private ChassisState chassisState;
     private ToolState toolState;
-
+    private GameState gameState;
     private Sensor sensor;
     private Chassis chassis;
+
+    private int activeCount = 0;
+    private int maxCount = 1;
+
+    private int pointerPosition;
+
     #endregion
 
     #region MonoBehaviour Methods
@@ -92,8 +109,16 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
-        ProcessMovement();
+        switch (gameState)
+        {
+            case GameState.MOVEMENT:
+                DoMovement();
+                break;
+            case GameState.SELECTION:
+                DoSelection();
+                break;
+        }
+
         UpdateUI();
         UpdateMap();
 
@@ -147,10 +172,42 @@ public class GameController : MonoBehaviour
         SensorText[(int)sensorState].color = ActiveColour;
         ChassisText[(int)chassisState].color = ActiveColour;
         ToolText[(int)toolState].color = ActiveColour;
+
+        if (pointerPosition < 4)        // is pointing at a Sensor
+        {
+            PointerText.transform.position = new Vector2(PointerText.transform.position.x,
+                                                         SensorText[pointerPosition].transform.position.y);
+        }
+        else if (pointerPosition < 8)   // is pointing at a Chassis
+        {
+            PointerText.transform.position = new Vector2(PointerText.transform.position.x,
+                                                         ChassisText[pointerPosition - 4].transform.position.y);
+        }
+        else                            // pointing at a Tool
+        {
+            PointerText.transform.position = new Vector2(PointerText.transform.position.x,
+                                                         ToolText[pointerPosition - 8].transform.position.y);
+        }
+
+        CountText.text = String.Format("ACTIVE  {0}\r\nMAX     {1}", activeCount, maxCount);
+        if (activeCount > maxCount)
+        {
+            CountText.color = ErrorColour;
+        }
+        else
+        {
+            CountText.color = InactiveColour;
+        }
     }
 
-    private void ProcessMovement()
+    private void DoMovement()
     {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SetState(GameState.SELECTION);
+            return;
+        }
+
         int new_x_pos = PlayerXPos;
         int new_y_pos = PlayerYPos;
         int forward_move = 0;
@@ -220,70 +277,89 @@ public class GameController : MonoBehaviour
         }
     }
 
-    private void ProcessInput()
+    private void DoSelection()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            sensor = new DirectionalSensor(TileMap);
-            sensorState = SensorState.BASIC;
-            headSpriteArray = spriteDefinitions.EGADirectionalHead;
+            if (activeCount <= maxCount)
+                SetState(GameState.MOVEMENT);
+    }
+        if (Input.GetKeyDown(KeyCode.DownArrow) | Input.GetKeyDown(KeyCode.S))
+        {
+            pointerPosition += 1;
+            if (pointerPosition == 12)
+            {
+                pointerPosition = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) | Input.GetKeyDown(KeyCode.W))
+        {
+            pointerPosition -= 1;
+            if (pointerPosition == -1)
+            {
+                pointerPosition = 11;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            sensor = new OmniSensor(TileMap);
-            sensorState = SensorState.OMNI;
-            headSpriteArray = spriteDefinitions.EGAOmniHead;
-        }
+            switch (pointerPosition)
+            {
+                case 0:
+                    SetSensor(SensorState.BASIC);
+                    break;
+                case 1:
+                    SetSensor(SensorState.OMNI);
+                    break;
+                case 2:
+                    SetSensor(SensorState.INFRARED);
+                    break;
+                case 3:
+                    SetSensor(SensorState.LONGRANGE);
+                    break;
+                case 4:
+                    SetChassis(ChassisState.BASIC);
+                    break;
+                case 5:
+                    SetChassis(ChassisState.SILENT);
+                    break;
+                case 6:
+                    SetChassis(ChassisState.FAST);
+                    break;
+                case 7:
+                    SetChassis(ChassisState.OFFROAD);
+                    break;
+                case 8:
+                    SetTool(ToolState.NONE);
+                    break;
+                case 9:
+                    SetTool(ToolState.LASER);
+                    break;
+                case 10:
+                    SetTool(ToolState.ACTUATOR);
+                    break;
+                case 11:
+                    SetTool(ToolState.PUSHER);
+                    break;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            sensor = new IRSensor(TileMap);
-            sensorState = SensorState.INFRARED;
-            headSpriteArray = spriteDefinitions.EGAIRHead;
+            activeCount = 0;
+            if (sensorState > SensorState.BASIC)
+                activeCount++;
+            if (chassisState > ChassisState.BASIC)
+                activeCount++;
+            if (toolState > ToolState.NONE)
+                activeCount++;
         }
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            sensor = new LongRangeSensor(TileMap);
-            sensorState = SensorState.LONGRANGE;
-            headSpriteArray = spriteDefinitions.EGALongRangeHead;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            chassisState = ChassisState.BASIC;
-            chassis = new Chassis(chassisState);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            chassisState = ChassisState.SILENT;
-            chassis = new Chassis(chassisState);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            chassisState = ChassisState.FAST;
-            chassis = new Chassis(chassisState);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            chassisState = ChassisState.OFFROAD;
-            chassis = new Chassis(chassisState);
-        }
-
     }
 
     private void InitializePlayer()
     {
         direction = Direction.NORTH;
-        sensorState = SensorState.BASIC;
-        sensor = new DirectionalSensor(TileMap);
-        headSpriteArray = spriteDefinitions.EGADirectionalHead;
-        chassisState = ChassisState.BASIC;
-        chassis = new Chassis(chassisState);
-        toolState = ToolState.NONE;
+        SetSensor(SensorState.BASIC);
+        SetChassis(ChassisState.BASIC);
+        SetTool(ToolState.NONE);
+        gameState = GameState.MOVEMENT;
     }
 
     private void InitializeUI()
@@ -317,7 +393,61 @@ public class GameController : MonoBehaviour
             RenderMaterial.mainTextureOffset = new Vector2(0, (1 - pixelRatioAdjustment) / 2);
             WorldCamera.orthographicSize = TargetX / 2;
         }
+
+        pointerPosition = 0;
+        PointerText.color = InactiveColour;
     }
 
+    private void SetChassis(ChassisState state)
+    {
+        chassisState = state;
+        chassis = new Chassis(state);
+    }
+
+    private void SetSensor(SensorState state)
+    {
+        sensorState = state;
+        switch (state)
+        {
+            case SensorState.BASIC:
+                sensor = new DirectionalSensor(TileMap);
+                headSpriteArray = spriteDefinitions.EGADirectionalHead;
+                break;
+            case SensorState.OMNI:
+                sensor = new OmniSensor(TileMap);
+                headSpriteArray = spriteDefinitions.EGAOmniHead;
+                break;
+            case SensorState.INFRARED:
+                sensor = new IRSensor(TileMap);
+                headSpriteArray = spriteDefinitions.EGAIRHead;
+                break;
+            case SensorState.LONGRANGE:
+                sensor = new LongRangeSensor(TileMap);
+                headSpriteArray = spriteDefinitions.EGALongRangeHead;
+                break;
+        }
+    }
+
+    private void SetState(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.MOVEMENT:
+                Debug.Log("Entering movement state");
+                PointerText.gameObject.SetActive(false);
+                break;
+            case GameState.SELECTION:
+                Debug.Log("Entering selection state");
+                pointerPosition = (int)sensorState;
+                PointerText.gameObject.SetActive(true);
+                break;
+        }
+        gameState = state;
+    }
+
+    private void SetTool(ToolState state)
+    {
+        toolState = state;
+    }
     #endregion
 }
